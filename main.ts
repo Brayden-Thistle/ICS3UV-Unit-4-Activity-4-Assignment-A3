6426e26954368869bdbe19c69ad1d9cb867b2a21
Brayden Thistle <bthistle@huronperthcatholic.ca>
2026-01-08 14:53:51 +0000

    created program to prompt the user for a sentence. it will then ask for a word in the sentence and if its right or wrong then itll display something according to your answer

--- main.ts
+++ main.ts
@@ -2,21 +2,27 @@
  * @author Brayden Thistle
  * @version 1.0.0
  * @date 2026-01-07
- * @fileoverview this program 
+ * @fileoverview this program will prompt the user for a sentence and then ask for a word insie the sentence. if its found then you will be told. if it isnt found you will also be told.
  */
 
+//variables
 let sentence: string = ""
-let guess1: string;
-let answer2: string = "" 
+let guess1: string
+//array
+let answer2: string[];
 
+//prompts
 sentence = String(prompt("Please enter a sentence:"));
 guess1 = String(prompt("Please enter a word to search for in your sentence:"));
 
-answer2 = sentence
+//redeclaring answer2
+answer2 = sentence.split(" ");
 
-if (answer2 === guess1) {
+//if else statement
+if (answer2.includes(guess1)) {
   console.log(`Yay, you guessed the right answer.`);
 } else {
   console.log("Incorrect, try again.");
 }
 
+console.log("\nDone.");
